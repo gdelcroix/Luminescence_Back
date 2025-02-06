3bf68507ec75a053938952e49f34e785f991ce06
gdelcroix <4097422+gdelcroix@users.noreply.github.com>
2025-02-06 11:43:21 +0100

    passage de la cnx bdd en pool

--- config/bdd.js
+++ config/bdd.js
@@ -3,15 +3,19 @@ const dotenv = require('dotenv');
 dotenv.config(); 
 
 // Configuration de la connexion à la base de données
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.HOST, 
     user: process.env.USER, 
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    multipleStatements: true,  // Pour exécuter des requêtes SQL multi-lignes
 });
 
 // Connexion à la base de données
-connection.connect((err) => {
+connection.getConnection((err) => {
     if (err) throw err;
     console.log('Appel bdd');});
 
